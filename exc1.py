4bf30fd98d5c4c69ec286b72a8d4524037b8b5f1
caioonofre <protectcaio@gmail.com>
2026-03-05 15:53:39 -0300

    exercicio 1 resolvido

---
+++ exc1.py
@@ -0,0 +1,7 @@
+print("Multiplicação de dois numeros: \n")
+number1 = int(input("Insira o primeiro numero para multiplicação: "))
+number2 = int(input("Insira o segundo numero para multiplicação: "))
+
+print(f'O resultado de {number1} X {number2} é: {number1*number2}')
+
+
