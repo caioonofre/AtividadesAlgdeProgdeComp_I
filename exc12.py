8575ada79c8b06427408e97ac409f233706cf0e1
caioonofre <protectcaio@gmail.com>
2026-03-06 09:06:26 -0300

    Exercício 12 resolvido

---
+++ exc12.py
@@ -0,0 +1,14 @@
+# Faça um algoritmo que leia o salário bruto mensal de um funcionário, calcule e
+# mostre os valores conforme modelo abaixo:
+# Salário Bruto :R$
+# (-) IR (15%) :R$
+# (-) INSS (11%) :R$
+# (-) Sindicato (3%) :R$
+# Salário Líquido :R$
+
+print("A calculadora do seu salário!")
+
+salario_bruto = float(input("Insira seu salário bruto: "))
+salario_liuqido = salario_bruto - ((salario_bruto*0.15) + (salario_bruto*0.11) + (salario_bruto*0.03))
+
+print(f'\nO IR(Imposto de renda) descontado do seu salário é: R${salario_bruto*0.15}\nO INSS descontado do seu salário é de: R${salario_bruto*0.11}\nO valor descontado pelo Síndicato do seu salário é: R${salario_bruto*0.03}\n\nO valor que Sobrou(salário líquido) é de: R${salario_liuqido}')
