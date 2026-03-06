3f362ce5b363e1c6dfce4304d0414427d15e883e
caioonofre <protectcaio@gmail.com>
2026-03-05 21:06:42 -0300

    exercício 7 resolvido

---
+++ exc7.py
@@ -0,0 +1,15 @@
+#variaveis
+
+a = int(input("Insira o valor de A: "))
+b = int(input("Insira o valor de B: "))
+c = int(input("Insira o valor de C: "))
+d = int(input("Insira o valor de D: "))
+e = int(input("Insira o valor de E: "))
+f = int(input("Insira o valor de F: "))
+
+x = round((c*e - b*f) / (a*e - b*d), 1)
+y = round((a*f - c*d) / (a*e - b*d), 1)
+
+result = (x, y)
+
+print("\nO resultado é: ", result)
