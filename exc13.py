dcefe16239f96abeffee0355d5915f366fdd33d2
caioonofre <protectcaio@gmail.com>
2026-03-06 09:28:11 -0300

    feat: alteração do ex13

--- exc13.py
+++ exc13.py
@@ -1,11 +1,3 @@
-# Maria quer saber quantos litros de gasolina precisa colocar em seu carro e quanto
-# vai gastar para fazer uma viagem até a casa de sua irmã. Faça um algoritmo que
-# leia:
-# • A distância da casa de Maria até sua irmã;
-# • O consumo do carro de Maria (KM rodados / litro);
-# • O preço da gasolina (litro).
-# E mostre as informações que Maria necessita.
-
 print("Calculador de gosolina!\n")
 
 distancia = float(input("Insira a distância que você deseja chegar[EM KILOMETROS KM]: "))
