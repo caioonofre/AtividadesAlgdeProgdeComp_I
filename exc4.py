1392e6b3cc5c99fe273fd300bd3c32db27c08bf5
caioonofre <protectcaio@gmail.com>
2026-03-09 09:04:45 -0300

    feat: adicionando biblioteca math no exc4

--- exc4.py
+++ exc4.py
@@ -1,6 +1,7 @@
+import math;
 print("Calcular o volume de uma lata de óleo!")
 
-raio = float(input("Insira o raio da lata: \n"))
-altura = float(input("Insira a altura: \n"))
-
-print(f'O valor do volume da lata é: {3.14 * (raio*raio) * altura}')
+raio = float(input("\nInsira o raio da lata[cm]: "))
+altura = float(input("\nInsira a altura[cm]: "))
+volume = round(math.pi * (raio*raio) * altura, 2)
+print(f'O valor do volume da lata é: {volume} cm3 ou {round(volume/1000, 2)} Litros')
