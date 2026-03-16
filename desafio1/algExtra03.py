111116c93051cbc18ae2857a712b091107d033d1
caioonofre <protectcaio@gmail.com>
2026-03-16 08:49:49 -0300

    add: exercício do desafio 03 resolvido

---
+++ desafio1/algExtra03.py
@@ -0,0 +1,7 @@
+print("Medidas de um retângulo!\n")
+
+base = float(input("Insira o valor do base do retângulo[cm]: "))
+altura = float(input("Insira o valor da altura do retângulo[cm]: "))
+
+print(f'\nO valor do perímetro do retângulo é: {2*(base+altura)} cm')
+print(f'\nO valor da área é de {base*altura} cm3')
