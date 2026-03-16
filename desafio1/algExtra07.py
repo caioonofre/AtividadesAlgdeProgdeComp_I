d8fecab7b45557c5c7543792bdf0d97374d3f66a
caioonofre <protectcaio@gmail.com>
2026-03-16 09:21:46 -0300

    feat: Exercício 07 alterado

--- desafio1/algExtra07.py
+++ desafio1/algExtra07.py
@@ -1,4 +1,3 @@
-# 7. Construir um algoritmo para ler o raio de uma circunferência e mostrar o perímetro ( 2 x π x raio ) e a área ( π x raio2). Utilize o π como constante.
 print("Medidas de uma cricunferência!\n")
 
 raio = float(input("Insira o valor do raio da circunferência[cm]: " ))
