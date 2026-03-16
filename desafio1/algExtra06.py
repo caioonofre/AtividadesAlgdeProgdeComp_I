24e7ab8515b89a213594ef39ebf9d386b2222fd8
caioonofre <protectcaio@gmail.com>
2026-03-16 09:11:10 -0300

    feat: exercicio 06 do desafio alterado

--- desafio1/algExtra06.py
+++ desafio1/algExtra06.py
@@ -1,6 +1,3 @@
-# Criar um algoritmo para ler a base e a altura de um triângulo e
-# mostrar a sua área ( ( base x altura ) / 2 ).
-
 print("Medidas de um triângulo!\n")
 
 base = float(input("Insira a base do triângulo[cm]: "))
