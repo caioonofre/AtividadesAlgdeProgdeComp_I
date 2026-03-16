8af06dae1a60725632074c781404e5985f27c327
caioonofre <protectcaio@gmail.com>
2026-03-16 08:44:14 -0300

    feat: alteração no arquivo algExtra02.py

--- desafio1/algExtra02.py
+++ desafio1/algExtra02.py
@@ -1,6 +1,3 @@
-# 2. Elaborar um algoritmo para ler o nome e a quantidade de filhos de uma pessoa.
-# Mostrar a mensagem: “Fulano tem 5 filhos.”
-
 nome = input("Qual seu nome?: ")
 filhos = int(input("Quantos filhos voce tem?: "))
 
