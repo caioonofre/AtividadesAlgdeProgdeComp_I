31ea2e214e8c94e141dc9a42141fe0d8570c94e4
caioonofre <protectcaio@gmail.com>
2026-03-16 09:05:16 -0300

    add: exercicio 5 resolvido

---
+++ desafio1/algExtra05.py
@@ -0,0 +1,7 @@
+print("Divisão de dois números!\n")
+
+num1 = int(input("Insira o primeiro número[Dividendo]: "))
+num2 = int(input("Insira o segundo número[Divisor]: "))
+
+print(f'\nO quociente(resultado) da divisão {num1}/{num2} é: {num1//num2}')
+print(f'O resto da divisão {num1}/{num2} é: {num1%num2}')
