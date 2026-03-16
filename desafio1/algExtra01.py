be2cf40fd877fba5f9b1de86d7f49e2749b91f9c
caioonofre <protectcaio@gmail.com>
2026-03-16 08:56:48 -0300

    feat: pequena alteração no exercicios 1 e 3 do desafio

--- desafio1/algExtra01.py
+++ desafio1/algExtra01.py
@@ -3,4 +3,4 @@ print("Transformador de medidas de um quadrado!")
 lado = float(input("Insira o valor do lado do quadrado[cm]: "))
 
 print(f'\nO valor do perímetro é {lado*4} cm')
-print(f'\nO valor da área é de {lado**2} cm3')
+print(f'\nO valor da área é de {lado**2} cm2')
--- desafio1/algExtra03.py
+++ desafio1/algExtra03.py
@@ -4,4 +4,4 @@ base = float(input("Insira o valor do base do retângulo[cm]: "))
 altura = float(input("Insira o valor da altura do retângulo[cm]: "))
 
 print(f'\nO valor do perímetro do retângulo é: {2*(base+altura)} cm')
-print(f'\nO valor da área é de {base*altura} cm3')
+print(f'\nO valor da área é de {base*altura} cm2')
