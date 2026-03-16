abcd970b166d36ca9ef4af9131044dbe353c35fd
caioonofre <protectcaio@gmail.com>
2026-03-16 08:57:07 -0300

    add: exercicio 4 do desafio resolvido

---
+++ desafio1/algExtra04.py
@@ -0,0 +1,6 @@
+print("Medidas de um cubo!")
+
+lado = float(input("Insira o valor do lado de um cubo[cm]: "))
+
+print(f'\nO valor da área do cubo é: {6*(lado**2)} cm2')
+print(f'O valor do volume do cubo é: {lado**3} cm3')
